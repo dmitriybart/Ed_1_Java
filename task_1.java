9df1590e15cf4ebad33c145902af2f859e042c17
Dmitriy B <kupperfeet@gmail.com>
2023-02-17 19:49:02 +0300

    first commit

---
+++ task_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class task_1 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 1, 0, 1, 1,1};
+        int count = 0;
+        32.28
+
+
+
+    }
+    
+}
